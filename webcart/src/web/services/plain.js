83891e2d8a7809385f65a1a2c6149dd6ce555ef2
Cendey <cendey@yeah.net>
2016-12-30 14:10:27 +0800

    Commit RESTful client request.

--- webcart/src/web/services/plain.js
+++ webcart/src/web/services/plain.js
@@ -8,3 +8,11 @@
  * @since 12/26/2016
  * @version 1.0
  */
+const httpModule = require('./../modules/http-module.js');
+const http = require("http");
+const port = 8180;
+const host = "127.0.0.1";
+
+http.createServer(httpModule.handleRequest).listen(port, host);
+
+console.log(`Started Node.js http server at ${host}:${port}`);
